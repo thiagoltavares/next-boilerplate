b14c8eb1d676e72f6e57b41290e13ca68ed1c4f5
Thiago Tavares <thiagodevtavares@gmail.com>
2021-05-18 21:08:48 +0100

    Lesson 26

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>React Avançado Boilerplate</title>
         <link rel="shortcut icon" href="img/icon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project to work with Typescript, React, NextJS and Styled Components"
